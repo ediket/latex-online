49b919e5937c804cebd6a65e097db42358a4d4af
Jungwoo Lee <jeong1135@gmail.com>
2018-06-19 20:45:52 -0700

    Changing the whole thing into post

--- app.js
+++ app.js
@@ -67,8 +67,10 @@ async function handleResult(res, preparation, force, downloadName) {
         return;
     }
     var compilation = latexOnline.compilationWithFingerprint(request.fingerprint);
-    if (force && compilation)
-        latexOnline.removeCompilation(compilation);
+    if (force && compilation) {
+      latexOnline.removeCompilation(compilation);
+    }
+
     compilation = latexOnline.getOrCreateCompilation(request, downloader);
     await compilation.run();
 
@@ -78,93 +80,27 @@ async function handleResult(res, preparation, force, downloadName) {
     if (compilation.userError) {
         sendError(res, compilation.userError);
     } else if (compilation.success) {
-        if (downloadName)
-          res.set('content-disposition', `attachment; filename="${downloadName}"`);
+        if (downloadName) {
+            res.set('content-disposition', `attachment; filename="${downloadName}"`);
+        }
         res.status(200).sendFile(compilation.outputPath(), {acceptRanges: false});
     } else {
         res.status(400).sendFile(compilation.logPath(), {acceptRanges: false});
     }
 }
 
-app.get('/version', (req, res) => {
-    res.json({
-        version: VERSION,
-        link: `http://github.com/aslushnikov/latex-online/commit/${VERSION}`
-    });
-});
-
-app.get('/health.json', (req, res) => {
-    if (!healthMonitor) {
-        sendError(res, 'ERROR: health monitor is not initialized.');
-        return;
-    }
-    var result = {
-        uptime: healthMonitor.uptime(),
-        health: healthMonitor.healthPoints()
-    };
-    res.json(result);
-});
-
-app.get('/health', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'health.html'));
-});
-
-app.get('/pending', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pending.html'));
-});
-
-var pendingTrackIds = new Set();
-app.get('/compile', async (req, res) => {
-    // Do not leak too much memory if clients drop connections on redirect.
-    // if (pendingTrackIds.size > 10000)
-    //     pendingTrackIds.clear();
-    // var trackId = req.query.trackId;
-    // var isBrowser = !req.useragent.isBot;
-
-    // if (isBrowser && (!trackId || !pendingTrackIds.has(trackId))) {
-    //     trackId = Date.now() + '';
-    //     pendingTrackIds.add(trackId);
-    //     var query = Object.assign({}, req.query);
-    //     query.trackId = trackId;
-    //
-    //     var search = Object.keys(query).map(key => `${key}=${encodeURIComponent(query[key])}`).join('&');
-    //     res.redirect(307, `/pending?${search}`);
-    //     return;
-    // }
-    // pendingTrackIds.delete(trackId);
-
+app.post('/compile', async (req, res) => {
     var forceCompilation = req.query && !!req.query.force;
     var command = req.query && req.query.command ? req.query.command : 'pdflatex';
     command = command.trim().toLowerCase();
     var preparation;
-    if (req.query.text) {
-        preparation = await latexOnline.prepareTextCompilation(req.query.text, command);
-    } else if (req.query.url) {
-        preparation = await latexOnline.prepareURLCompilation(req.query.url, command);
-    } else if (req.query.git) {
-        var workdir = req.query.workdir || '';
-        preparation = await latexOnline.prepareGitCompilation(req.query.git, req.query.target, 'master', command, workdir);
+
+    if (req.body.text) {
+        preparation = await latexOnline.prepareTextCompilation(req.body.text, command);
     }
+
     if (preparation)
         handleResult(res, preparation, forceCompilation, req.query.download);
     else
         sendError(res, 'ERROR: failed to parse request: ' + JSON.stringify(req.query));
-});
-
-var multer  = require('multer')
-var upload = multer({ dest: '/tmp/file-uploads/' })
-app.post('/data', upload.any(), async (req, res) => {
-    if (!req.files || req.files.length !== 1) {
-        sendError(res, 'ERROR: files are not uploaded to server.');
-        return;
-    }
-    var command = req.query && req.query.command ? req.query.command : 'pdflatex';
-    command = command.trim().toLowerCase();
-    var file = req.files[0];
-    var preparation = await latexOnline.prepareTarballCompilation(file.path, req.query.target, command);
-    if (preparation)
-        await handleResult(res, preparation, true /* force */, null /* downloadName */);
-    else
-        sendError(res, 'ERROR: failed to process file upload!');
-    utils.unlink(file.path);
 });
